9fb4a045cf5d40e9d2d6022342356e7d36355d3f
SangKil Park <ht1911@gmail.com>
2018-05-15 19:50:43 +0900

    PubSub

--- src/main/java/com/power/study/PubSub.java
+++ src/main/java/com/power/study/PubSub.java
@@ -1,17 +1,57 @@
 package com.power.study;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+@Slf4j
 public class PubSub {
 
     public static void main(String[] args) {
 
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            public void subscribe(Subscriber<? super Integer> subscriber) {
-                subscriber.onSubscribe(new Subscription() {
+        Publisher<Integer> pub = iterPub();
+
+
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            public void onNext(Integer i) {
+                log.debug("onNext", i);
+
+            }
+
+            public void onError(Throwable t) {
+                log.debug("onError");
+            }
+
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        };
+
+        pub.subscribe(sub);
+    }
+
+    private static Publisher<Integer> iterPub() {
+        return new Publisher<Integer>() {
+            Iterable<Integer> iter = Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList());
+
+            public void subscribe(Subscriber<? super Integer> sub) {
+                sub.onSubscribe(new Subscription() {
                     public void request(long l) {
+                        try {
+                            iter.forEach( s -> sub.onNext(s));
+                            sub.onComplete();
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
 
                     }
 
@@ -21,25 +61,5 @@ public class PubSub {
                 });
             }
         };
-
-
-        Subscriber<Integer> sub = new Subscriber<Integer>() {
-            public void onSubscribe(Subscription subscription) {
-
-            }
-
-            public void onNext(Integer integer) {
-
-            }
-
-            public void onError(Throwable throwable) {
-
-            }
-
-            public void onComplete() {
-
-            }
-        };
-
     }
 }
