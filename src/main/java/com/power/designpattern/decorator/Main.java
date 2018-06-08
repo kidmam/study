df37b9da2eb03d326bec6ea92ee22cf9e44d5c06
SangKil Park <ht1911@gmail.com>
2018-06-08 17:32:02 +0900

    Design Pattern - Decorator  - 장식과 내용물을 동일시하기

---
+++ src/main/java/com/power/designpattern/decorator/Main.java
@@ -0,0 +1,30 @@
+package com.power.designpattern.decorator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display b1 = new StringDisplay("Hello, Wrold.");
+        b1.show();
+
+        Display b2 = new SideBorder(b1, '#');
+        b2.show();
+
+        Display b3 = new FullBorder(b2);
+        b3.show();
+
+        Display b4 = new SideBorder(
+                new FullBorder(
+                        new FullBorder(
+                                new SideBorder(
+                                        new FullBorder(
+                                                new StringDisplay("안녕하세요")
+                                        ),
+                                        '*'
+                                )
+                        )
+                )
+                ,'/'
+        );
+        b4.show();
+    }
+}
