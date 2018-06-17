70896eb4fcba4ce558e8a6a23c40584de2eeb330
SangKil Park <ht1911@gmail.com>
2018-06-18 05:23:29 +0900

    재귀함수 - 함수가 자기 자신을 호출해서 하위 작업을 수행하는 것

---
+++ src/main/java/com/power/algorithms/Main.java
@@ -0,0 +1,21 @@
+package com.power.algorithms;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    int Fact(int n) {
+        if ( n == 0 )
+            return 1;
+        else if ( n == 1 )
+            return 1;
+        else
+            return n * Fact( n - 1 );
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        log.info(Integer.toString(m.Fact(5)));
+    }
+}
