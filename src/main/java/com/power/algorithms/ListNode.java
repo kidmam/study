413bdf2dfa87905d45752a8602a87fd1002ad92c
SangKil Park <ht1911@gmail.com>
2018-06-24 19:10:39 +0900

    단일 연결 리스트

---
+++ src/main/java/com/power/algorithms/ListNode.java
@@ -0,0 +1,22 @@
+package com.power.algorithms;
+
+public class ListNode {
+    private int data;
+    private ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
