941290b3c31504b0856f4316cda3b673393ec9d9
SangKil Park <ht1911@gmail.com>
2018-06-26 10:19:38 +0900

    단일 연결 리스트의 중간에 노드 삽입하기

--- src/main/java/com/power/algorithms/ListNodeMain.java
+++ src/main/java/com/power/algorithms/ListNodeMain.java
@@ -12,6 +12,31 @@ public class ListNodeMain {
         return length;
     }
 
+    ListNode InsertInLinkdedList(ListNode headNode, ListNode nodeToInsert, int position) {
+        if (headNode == null) {
+            return nodeToInsert;
+        }
+        int size = ListLength(headNode);
+        if (position > size + 1 || position < 1) {
+            System.out.println("Position of node to insert is invalid. The valid inputs are 1 to " + (size + 1));
+        }
+        if (position == 1) {
+            nodeToInsert.setNext(headNode);
+            return nodeToInsert;
+        } else {
+            ListNode previousNode = headNode;
+            int count = 1;
+            while(count < position - 1) {
+                previousNode = previousNode.getNext();
+                count++;
+            }
+            ListNode currentNode = previousNode.getNext();
+            nodeToInsert.setNext(currentNode);
+            previousNode.setNext(nodeToInsert);
+        }
+        return headNode;
+    }
+
     public static void main(String[] args) {
 
     }
